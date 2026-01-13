0995a346c0c1f9836dc7cb87ac47c2cb4785472e
James Vongphasouk <james.vongpha@gmail.com>
2026-01-12 21:30:28 -0700

    feat: initial model

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
 
-func main(){
-	fmt.Println("Hello, world!")
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	board [][]int
+	solution [][]int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	return ""
+}
+
+func initialModel() model {
+	// example where filling in the middle square is the solution
+	return model{
+		board: [][]int{
+			{0, 0, 0},
+		 	{0, 0, 0},
+			{0, 0, 0}},
+		
+		solution: [][]int{
+			{0, 0, 0},
+			{0, 1, 0},
+			{0, 0, 0}},
+	}
+}
+
+func main(){	
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error running program: %v\n", err)
+		os.Exit(1)
+	}
 }
