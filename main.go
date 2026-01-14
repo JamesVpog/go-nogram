38c8e0cacff7fb67bc73a0d6b89771eadb84a54a
James Vongphasouk <82847301+JamesVpog@users.noreply.github.com>
2026-01-14 15:53:48 -0700

    Feature/tui styling, redone (#6)
    * chore: clean todo
    
    * feat: inital grid of zeroes
    
    * feat: styles for highlighted cell and fix update to reflect view indices
    
    * feat: fix x,y to col,row
    
    * feat: center and fill with colors
    
    * feat: render cell logic refactor and match solution board

--- main.go
+++ main.go
@@ -56,8 +56,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor.col--
 			}
 		case " ", "enter":
-			// fill in the square at that position
-			m.board[m.cursor.row][m.cursor.col] = 1	
+			// fill only if the solution allows to be filled
+			if m.solution[m.cursor.row][m.cursor.col] == 1 {	
+				// user was right!
+				m.board[m.cursor.row][m.cursor.col] = 1	 
+			} else {
+				// user got it wrong!
+				m.board[m.cursor.row][m.cursor.col] = 3
+			}
 		case "x", "X":
 			// draw x in the square at that position 
 			m.board[m.cursor.row][m.cursor.col] = 2
@@ -68,6 +74,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // styles for the view
 var (	
+	baseBlack 	= lipgloss.Color("0")
+	
+	cyan	= lipgloss.Color("6")
 	red    = lipgloss.Color("1")
 	green  = lipgloss.Color("2")
 	purple = lipgloss.Color("5")
@@ -75,37 +84,45 @@ var (
 	cellWidth = 10
 	cellHeight = 5
 	
-	cellStyle = lipgloss.NewStyle().
+	baseStyle = lipgloss.NewStyle().
 	    Width(cellWidth).
 	    Height(cellHeight).
 	    Align(lipgloss.Center, lipgloss.Center).
-	    Border(lipgloss.NormalBorder())
-	
-	filledStyle = cellStyle.Copy().
-		Background(red).
-		Foreground(red)
-	
-	highlightedCell = cellStyle.Copy().
-		Background(lipgloss.Color(green))
+	    Border(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("7"))
 )
 
 
 func (m model) View() string {
 	
-	translateDataToCellStyle := func(data int) string {
-		// switch on the data to return the appropriate 3 states of each nonogram cell
-		switch data {
-		case 0:
-			// not filled in, empty spaces
-			return cellStyle.Render("")
+	renderCell := func(r, c int) string {
+		isFocused := (r == m.cursor.row && c == m.cursor.col)
+		currentState := m.board[r][c]
+
+		style := baseStyle
+		// 1. check if focused
+		if isFocused {
+			style = style.BorderForeground(cyan).Border(lipgloss.ThickBorder())
+		}
+		
+		// switch on the data to return the appropriate 4 states of each nonogram cell
+		// 0. empty, 
+		// 1. filled, 
+		// 2. user-x'd out, 
+		// 3. incorrectly filled
+		
+		switch currentState {
 		case 1:
 			//filled in
-			return filledStyle.Render("")
+			style = style.Foreground(green).Background(green)
 		case 2:
 			// marked as x by the user
-			return cellStyle.Render("x")
+			return style.Render("x")
+		case 3:
+			// fileld in but wrong!
+			style = style.Foreground(red).Background(red)
 		}
-		return ""
+		return style.Render("")
 	}
 	// loop through the m.board and render each cell, store in renderedCells
 	// use joinHorizontal to make a row, stored in rows
@@ -113,13 +130,7 @@ func (m model) View() string {
 	for r := range m.board {
 		var renderedCells []string
 		for c := range m.board[r] {
-			// strForm := strconv.Itoa(m.board[r][c])
-			cell := translateDataToCellStyle(m.board[r][c])
-			
-			// if the cell is currently where the user is at highlight it!
-			if r == m.cursor.row && c == m.cursor.col {
-				cell = highlightedCell.Render("")
-			}
+			cell := renderCell(r, c)
 			renderedCells = append(renderedCells, cell)
 		}
 		row := lipgloss.JoinHorizontal(lipgloss.Top, renderedCells...)
@@ -129,6 +140,7 @@ func (m model) View() string {
 	// send rows to joinVertical to make the grid
 	grid := lipgloss.JoinVertical(lipgloss.Left, rows...)
 	
+	//TODO: instructions on the bottom?
 	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, grid)
 }
 
